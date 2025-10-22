8a647da56a97d186002be29f5e75a75a4972f983
aleynaaley <aleyna.erkulll@gmail.com>
2025-10-22 23:44:42 +0300

    Added blog serbia

--- src/pages/blog/Sirbistan.jsx
+++ src/pages/blog/Sirbistan.jsx
@@ -8,26 +8,59 @@ function Sirbistan() {
     </button>
       <section className="min-h-screen bg-white py-16 px-6 sm:px-12">
         <div className="max-w-4xl mx-auto space-y-8">
-          <h1 className="text-4xl font-bold text-[#819A91] text-center mb-6">Sırbistan Gezisi</h1>
+          <h1 className="text-4xl font-bold text-[#819A91] text-center mb-6">Sırbistan ve Nikola Tesla</h1>
           {/* Banner görsel (sayfa başı) */}
           <img
-            src="/blogs_2/sırbistan.jpeg"
+            src="/blogs_2/sırbistan/s2.jpeg"
             className="w-full rounded-lg shadow-md" />
 
           <br></br>
           <p className="text-gray-800 text-lg leading-relaxed">
-            Merhaba!
-            Bu yazımda, arkadaşlarımla birlikte yaptığım Sırbistan
+            Merhaba! Balkan turumuz da rotamızı, Tuna ve Sava nehirlerinin kucaklaştığı, dinamik ve tarihi dokusuyla büyüleyen Sırbistan'a çeviriyoruz.
           </p>
 
-         
-          <p className="text-gray-800 text-lg leading-relaxed"></p>
+          <p className="text-gray-800 text-lg leading-relaxed">Sırbistan'daki ilk durağımız tabii ki başkent Belgrad oldu. Şehre akşam vardığımız için vakit kaybetmeden ilk olarak konaklayacağımız otele, Hotel Zira'ya yerleştik. Günün yorgunluğunu atmak ve şehri keşfetmeye başlamadan önce dinlenmek iyi geldi. Otelden bahsetmem gerekirse, konumu bence merkezi sayılabilecek güzel bir noktadaydı. Hemen yakınında bir otobüs durağının olması, ertesi gün şehri keşfetmek için ulaşım açısından büyük kolaylık sağlayacak gibi görünüyordu. </p>
 
-          <p className="text-gray-800 text-lg leading-relaxed"></p>
+          <div className="flex flex-wrap gap-4 justify-center mb-8">
 
-          <p className="text-gray-800 text-lg leading-relaxed"></p>
+            <img
+              src="/blogs_2/sırbistan/s3.jpeg"
+              className="w-full md:w-[48%] rounded-lg shadow-md"
+            />
+            <img
+              src="/blogs_2/sırbistan/s8.jpeg"
+              className="w-full md:w-[48%] rounded-lg shadow-md"
+            />
+          </div>
 
-          <p className="text-gray-800 text-lg leading-relaxed"></p>
+          <p className="text-gray-800 text-lg leading-relaxed">Odamıza yerleşip eşyalarımızı bıraktıktan sonra akşam yemeği için otelin restoranına indik. Otel genel olarak temizdi ve bu temizlik anlayışı restoranda da hissediliyordu. Yemeklere gelirsek; ana yemekler fena değildi, çeşitlilik belki biraz daha fazla olabilirdi ama iş tatlılara gelince durum tamamen değişti! Tatlı büfesi epey zengindi ve birbirinden farklı birçok seçenek vardı. Bu da benim gibi bir tatlıseveri mutlu etmeye yetti de arttı bile</p>
+
+          <p className="text-gray-800 text-lg leading-relaxed">Yemeğimizi bitirir bitirmez, günün yorgunluğuna aldırış etmeden Belgrad'ın gecesini merak edip hemen kendimizi dışarı attık. Rotamız, şehrin kalbinin attığı yer olarak bilinen meşhur Knez Mihailova Caddesi'ydi. Otele yakın duraktan otobüse bindik. Bu arada Sırbistan ile ilgili çok ilginç bir detay öğrendik; görünüşe göre ülkede birkaç özel hat dışında toplu taşıma kullanmak tamamen ücretsizmiş! Bu, şehri keşfetmek isteyen gezginler için gerçekten harika bir kolaylık.</p>
+
+          <video
+            src="/blogs_2/sırbistan/s6.mov"
+            autoPlay
+            muted
+            loop
+            className="w-81 mx-auto h-auto rounded-lg shadow-md my-6"
+            />
+
+          <p className="text-gray-800 text-lg leading-relaxed">Knez Mihailova'ya vardığımızda bizi cıvıl cıvıl bir atmosfer karşıladı. Akşam olmasına rağmen cadde şaşırtıcı derecede kalabalıktı; adeta bir insan seli vardı. Biz de bu kalabalığa karıştık, sağlı sollu sıralanmış mağazalara, dükkanlara göz attık, biraz alışveriş yerlerini gezdik. Caddenin ışıkları, kafelerden gelen müzik sesleri ve o canlı kalabalık, Belgrad'a harika bir "hoş geldin" demiş oldu. Gece biraz daha ilerleyip kalabalık yavaş yavaş seyrekleşmeye başlayınca, biz de ilk akşam gezintimizi sonlandırıp dinlenmek üzere otelimize döndük.</p>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-12">
+            <img
+              src="/blogs_2/sırbistan/s7.jpeg"
+              className="w-full h-auto rounded-lg shadow-md object-cover"
+            />
+            <img
+              src="/blogs_2/sırbistan/s4.jpeg"
+              className="w-full h-auto rounded-lg shadow-md object-cover"
+            />
+            <img
+              src="/blogs_2/sırbistan/s5.jpeg"
+              className="w-full h-auto rounded-lg shadow-md object-cover"
+            />
+          </div>
 
           <p className="text-gray-800 text-lg leading-relaxed"></p>
 
